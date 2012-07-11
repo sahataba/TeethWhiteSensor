15cbef37728e6b4076e9da87a5eeb8aba6c704da
Rudolf Markulin <rudolf.markulin@gmail.com>
2012-07-11 18:34:52 +0200

    Average color catwgory

---
+++ UIImage+AverageColor.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+AverageColor.h
+//  TeethWhiteSensor
+//
+//  Created by Rudolf Markulin on 7/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (AverageColor)
+
+@end
---
+++ UIImage+AverageColor.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+AverageColor.m
+//  TeethWhiteSensor
+//
+//  Created by Rudolf Markulin on 7/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIImage+AverageColor.h"
+
+@implementation UIImage (AverageColor)
+
+@end
